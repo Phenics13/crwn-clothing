0cab7b6651679e0b71e082364f124d4cc474a0fb
Phenics13 <optron920@gmail.com>
2022-12-28 10:58:44 +0200

    Hided scrollbars from cart-container and changed cart-icon svg

--- src/compenents/cart-dropdown/cart-dropdown.component.jsx
+++ src/compenents/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,3 @@
-import { useContext } from 'react';
-
 import Button from '../button/button.component';
 
 import './cart-dropdown.styles.scss';
@@ -9,7 +7,7 @@ const CartDropdown = () => {
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-
+        
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
